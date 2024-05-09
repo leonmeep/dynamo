5d3b4f0c97663e2c71f16202e40347d1a8632b8a
Leon Birch <leonbirch333@gmail.com>
2024-05-09 17:56:33 +0100

    fixed header link on the about page to redirect to the home page, may need changing to somewhere significant later

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php
 
-$heading = "<img src= /sg.png alt='Strange Garden'>";
+$heading = "<a href='index.php'><img src= /sg.png alt='Strange Garden'></a>";
 $title = "About Us";
 
 require 'views/about.view.php';
