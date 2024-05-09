c51969c21112427aa92944db3dfb4e0790be7598
Leon Birch <leonbirch333@gmail.com>
2024-05-09 14:18:41 +0100

    added linkedin link and icon to footer

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,7 +1,12 @@
-
-
 <footer>
-    <p>&copy; 2024 | Leon Birch Development | <a href="https://github.com/leonmeep/dynamo">GitHub</a></p>
+    <div class="flex justify-center space-x-2">
+        <p>© 2024 | Leon Birch Development | All rights reserved</p>
+        <a href="https://github.com/leonmeep/dynamo"><img src="/github-mark.png" alt="git hub logo link to repository"
+                                                          class="w-9 h-9"></a>
+        <a href="https://www.linkedin.com/in/leonbirch"><img src="/linkedinIcon.png" alt="facebook logo"
+                                                             class="object-contain h-9 w-9"></a>
+    </div>
+
 </footer>
 </body>
 </html>
