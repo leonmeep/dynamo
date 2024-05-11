91d245f75593214e432ac8dadd37a4eb1fe61628
Leon Birch <leonbirch333@gmail.com>
2024-05-11 15:21:06 +0100

    styled return home link

--- views/404.php
+++ views/404.php
@@ -12,7 +12,7 @@ require 'views/partials/header.php';
 </div>
 
 <p class="flex justify-center p-6 pr-6 pb-6 pl-6 pt-6 m-7 ml-7 mr-7 mt-7 mb-7">
-    <a href="/" class="text-blue-600 underline">Return to Home</a>
+    <a href="/" class="text-blue-600 underline hover:animate-pulse hover:text-green-600">Return to Home</a>
 </p>
 
 
