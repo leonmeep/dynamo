36dfdedc0ef3a3271fbcc0c76ef281d05f36aab1
Leon Birch <leonbirch333@gmail.com>
2024-05-11 16:36:54 +0100

    connected to database

--- index.php
+++ index.php
@@ -2,3 +2,19 @@
 
 require 'functions.php';
 require 'router.php';
+
+
+$dsn = 'mysql:host=localhost;dbname=strange-garden-ml;user=root;port=3306;charset=utf8mb4';
+new PDO($dsn);
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare('SELECT * FROM subscribers');
+$statement->execute();
+
+$subscribers = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($subscribers as $subscriber) {
+   echo "<li>{$subscriber['name']} - {$subscriber['email']}</li>";
+
+}
