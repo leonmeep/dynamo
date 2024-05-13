abbccd9ceda5c7bfd8874fe9d4d557764e4bc946
Leon Birch <leonbirch333@gmail.com>
2024-05-13 18:34:23 +0100

    split name and email validation

--- controllers/subscribe.php
+++ controllers/subscribe.php
@@ -12,17 +12,41 @@ $title = "Subscribe";
 
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
 
     $errors = [];
+//name validation
+    if (preg_match('/[^A-Za-z0-9 ]/', $_POST['name']))
+    {
+        $errors['name'] = 'Name should not contain special characters';
+    }
 
-    if (strlen($_POST['name']) < 1) {
-        $errors['name'] = 'You must enter a name';
+    if (strlen($_POST['name']) > 30  || strlen($_POST['name']) < 1) {
+        $errors['name'] = 'The name must be at least character and not longer than 30 characters';
+    }
+
+    if(empty($errors))
+    {
+        $db->query('SELECT * FROM subscribers WHERE name = :name', [
+            'name' => $_POST['name']
+        ]);
+
+    }
+
+//email validation
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+    {
         $errors['email'] = 'You must enter a valid email';
     }
+
+    if (strlen($_POST['email']) > 50 )
+    {
+        $errors['email'] = 'The email cannot be longer than 50 characters';
+    }
+
     if (empty($errors)) {
-        $db->query('INSERT INTO subscribers (name, email) VALUES (:name, :email)', [
-            'name' => $_POST['name'],
+        $db->query('INSERT INTO subscribers (name, email) VALUES ( :email)', [
             'email' => $_POST['email']
         ]);
     }
